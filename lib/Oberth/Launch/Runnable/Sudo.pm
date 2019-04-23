8ede5aa42bc70a8c5ad88e3e5107d974abe3aa54
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-04-23 02:41:30 -0500

    Check for NOPASSWD in sudo list output
    This means that it can run commands without a password.

--- lib/Oberth/Launch/Runnable/Sudo.pm
+++ lib/Oberth/Launch/Runnable/Sudo.pm
@@ -23,8 +23,30 @@ classmethod sudo_does_not_require_password() {
 	my ($stdout, $stderr, $exit) = capture {
 		system(qw(sudo -nv));
 	};
+
 	if( 0 != $exit && $stderr =~ /^sudo:/ ) {
-		warn "$stderr";
+		# Password has not been entered in prior to this.
+		warn "No sudo password entered in yet: $stderr";
+
+		# Try to see if NOPASSWD is in output
+		my $nopasswd = 0;
+		my ($sudo_l_stdout, $sudo_l_exit);
+		try {
+			local $SIG{ALRM} = sub { die "alarm\n" };
+			alarm 2;
+			($sudo_l_stdout, undef, $sudo_l_exit) = capture {
+				system(qw(sudo -l));
+			};
+			alarm 0;
+		} catch {
+			# Timed out.
+			$nopasswd = 0;
+			warn "Can not access sudo list without password";
+		};
+
+		$nopasswd = $sudo_l_stdout =~ m/\Q(ALL) NOPASSWD: ALL\E/s;
+
+		return 1 if $nopasswd;
 	}
 	return 0 == $exit;
 }
