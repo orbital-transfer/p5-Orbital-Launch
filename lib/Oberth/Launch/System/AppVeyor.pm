ce673206bc93df8ebc0a5a9f5b12dc22805cc254
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-07-27 00:46:05 -0500

    Add --noconfirm flag to pacman

--- lib/Oberth/Launch/System/AppVeyor.pm
+++ lib/Oberth/Launch/System/AppVeyor.pm
@@ -111,7 +111,7 @@ method _install() {
 curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz;
 curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig;
 pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz{.sig,};
-pacman -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz;
+pacman --noconfirm -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz;
 EOF
 			environment => $self->environment,
 		)
