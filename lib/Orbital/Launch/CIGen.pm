c883858c524f21da918fca991cb79e7b1e680086
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-12-04 18:02:09 -0600

    Add GitHub Actions CI file

--- lib/Orbital/Launch/CIGen.pm
+++ lib/Orbital/Launch/CIGen.pm
@@ -8,11 +8,14 @@ use CLI::Osprey;
 use Path::Tiny;
 use Orbital::Launch::CIGen::AppVeyor;
 use Orbital::Launch::CIGen::TravisCI;
+use Orbital::Launch::CIGen::GitHubActions;
 
 method run() {
 	my $paths_to_data = {
 		'appveyor.yml' => ${ Orbital::Launch::CIGen::AppVeyor->section_data('appveyor.yml') },
-		'.travis.yml'  => ${ Orbital::Launch::CIGen::TravisCI->section_data('.travis.yml') }
+		'.travis.yml'  => ${ Orbital::Launch::CIGen::TravisCI->section_data('.travis.yml') },
+		'.github/workflows/orbital-transfer.yml'  => ${ Orbital::Launch::CIGen::GitHubActions->section_data('orbital-transfer.yml') },
+		
 	};
 	while( my ($path, $data) = each %$paths_to_data ) {
 		my $path_obj = path($path);
