2959324594b5b4a5c74cd5f5eea91b05d9529aef
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-12-19 22:16:59 -0600

    GHA: Add step for cacheing

--- lib/Orbital/Launch/CIGen/GitHubActions.pm
+++ lib/Orbital/Launch/CIGen/GitHubActions.pm
@@ -34,6 +34,19 @@ jobs:
             joblabel: '(Coverage)'
     steps:
       - uses: actions/checkout@v2
+
+      - name: Cache Orbital
+        uses: actions/cache@v2
+        env:
+          cache-name: cache-orbital
+        with:
+          path:
+	    ../_orbital/author-local
+	    ../local
+          key: ${{ runner.os }}-build-${{ env.cache-name }}
+          restore-keys: |
+            ${{ runner.os }}-build-${{ env.cache-name }}-
+
       - name: Set up Orbital Transfer
         shell: bash
         run: |
